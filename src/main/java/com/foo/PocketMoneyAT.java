17e40616612e64ade49b57f9e3d93a36f5c689cb
thomas.hartthaler <thomas.hartthaler.ext@zeiss.com>
2016-03-29 16:14:01 +0200

    added and styled root container including summary table and insertion form

--- src/main/java/com/foo/PocketMoneyAT.java
+++ src/main/java/com/foo/PocketMoneyAT.java
@@ -1,9 +1,13 @@
 package com.foo;
 
+import com.foo.ui.MainView;
 import javafx.application.Application;
+import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.layout.StackPane;
 import javafx.stage.Stage;
+
+import java.io.IOException;
 
 public class PocketMoneyAT extends Application {
 
@@ -13,8 +17,14 @@ public class PocketMoneyAT extends Application {
 
     @Override
     public void start(Stage stage) throws Exception {
-        Scene scene = new Scene(new StackPane(), 800, 600);
+        Scene scene = new Scene(createRoot(), 800, 600);
+        scene.getStylesheets().add(getClass().getResource("application.css").toExternalForm());
         stage.setScene(scene);
+        stage.setTitle("pocketmoney-at");
         stage.show();
+    }
+
+    private Parent createRoot() throws IOException {
+        return new MainView().getRoot();
     }
 }
---
+++ src/main/java/com/foo/ui/MainView.java
@@ -0,0 +1,54 @@
+package com.foo.ui;
+
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.control.Button;
+import javafx.scene.control.TableView;
+import javafx.scene.layout.GridPane;
+
+import java.io.IOException;
+
+public class MainView {
+
+    @FXML
+    private Button insertButton;
+
+    @FXML
+    private Button summaryButton;
+
+    @FXML
+    private TableView<String> summaryTable;
+
+    @FXML
+    private GridPane insertionPane;
+
+    public Parent getRoot() throws IOException {
+        FXMLLoader fxmlLoader = new FXMLLoader();
+        fxmlLoader.setController(this);
+        fxmlLoader.setLocation(getClass().getResource(getClass().getSimpleName() + ".fxml"));
+
+        Parent parent = fxmlLoader.load();
+
+        initialize();
+
+        return parent;
+    }
+
+    @FXML
+    public void onInsert() {
+        insertionPane.setVisible(true);
+    }
+
+    @FXML
+    public void onSummary() {
+        insertionPane.setVisible(false);
+    }
+
+    private void initialize() {
+        insertionPane.managedProperty().bind(insertionPane.visibleProperty());
+        summaryTable.managedProperty().bind(summaryTable.visibleProperty());
+        summaryTable.visibleProperty().bind(insertionPane.visibleProperty().not());
+    }
+
+}
